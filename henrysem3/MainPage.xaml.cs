f817e90acd4c946e71a372ddda64618ec85d0570
master <master@SMLAB408PC03_22>
2022-04-23 11:17:32 -0500

    Agregar archivos de proyecto.

---
+++ henrysem3/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace henrysem3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnabrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtusuario.Text;
+            string contraseña = txtcontraseña.Text;
+            await Navigation.PushAsync(new sem3(usuario,contraseña));
+
+        }
+    }
+}
---
+++ henrysem3/sem3.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace henrysem3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class sem3 : ContentPage
+    {
+        public sem3(string usuario, string contraseña)
+        {
+            InitializeComponent();
+           // DisplayAlert("Datos que llegan", "el Usuario es: " + usuario ,"la contraseña es: " + contraseña, "cerrar");
+                  
+        }
+    }
+}
